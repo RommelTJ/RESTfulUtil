226bb6c240c9ce5d957b389901d0e9c3c65f5d85
Rommel Rico <rommeltj@gmail.com>
2014-02-11 12:43:58 -0800

    Formatting changes on SubmitMealPlan.java

--- src/main/java/edu/sandiego/restfulUtil/beans/SubmitMealPlan.java
+++ src/main/java/edu/sandiego/restfulUtil/beans/SubmitMealPlan.java
@@ -1,118 +1,83 @@
 package edu.sandiego.restfulUtil.beans;
 
 public class SubmitMealPlan {
-	
-	
     private String m_plan;
-	
 	private String p_term;
-	
 	private int p_mremain;
-	 
 	private int ov_pidm;
-	 
 	private float vcc;
-	 
 	private float vdd_40;
-	 
 	private float vdd_41;
-	 
 	private float vdd_45;
-	 
 	private String rdChoice;
-	 
 	private String t_type;
-	 
 	private int pidm;
 
 	public String getM_plan() {
 		return m_plan;
 	}
-
 	public void setM_plan(String m_plan) {
 		this.m_plan = m_plan;
 	}
-
 	public String getP_term() {
 		return p_term;
 	}
-
 	public void setP_term(String p_term) {
 		this.p_term = p_term;
 	}
-
-	public int getP_mremain() {
+    public int getP_mremain() {
 		return p_mremain;
 	}
-
-	public void setP_mremain(int p_mremain) {
+    public void setP_mremain(int p_mremain) {
 		this.p_mremain = p_mremain;
 	}
-
 	public int getOv_pidm() {
 		return ov_pidm;
 	}
-
 	public void setOv_pidm(int ov_pidm) {
 		this.ov_pidm = ov_pidm;
 	}
-
 	public float getVcc() {
 		return vcc;
 	}
-
 	public void setVcc(float vcc) {
 		this.vcc = vcc;
 	}
-
 	public float getVdd_40() {
 		return vdd_40;
 	}
-
 	public void setVdd_40(float vdd_40) {
 		this.vdd_40 = vdd_40;
 	}
-
 	public float getVdd_41() {
 		return vdd_41;
 	}
-
 	public void setVdd_41(float vdd_41) {
 		this.vdd_41 = vdd_41;
 	}
-
 	public float getVdd_45() {
 		return vdd_45;
 	}
-
 	public void setVdd_45(float vdd_45) {
 		this.vdd_45 = vdd_45;
 	}
-
 	public String getRdChoice() {
 		return rdChoice;
 	}
-
 	public void setRdChoice(String rdChoice) {
 		this.rdChoice = rdChoice;
 	}
-
 	public String getT_type() {
 		return t_type;
 	}
-
 	public void setT_type(String t_type) {
 		this.t_type = t_type;
 	}
-
 	public int getPidm() {
 		return pidm;
 	}
-
 	public void setPidm(int pidm) {
 		this.pidm = pidm;
 	}
-	
-	
 
 }
